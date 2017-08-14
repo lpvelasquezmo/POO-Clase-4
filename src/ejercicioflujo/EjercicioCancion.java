701bd2f5e6ee5c7709713ca0531f7ee501caa5ce
Estudiante <Estudiante@10.203.1.196>
2017-08-14 16:08:02 -0500

    ejercicio 1 de cancion POO clase

---
+++ src/ejercicioflujo/EjercicioCancion.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicioflujo;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class EjercicioCancion {
+    // atributos
+    public String titulo;
+    // metodos
+    public void sonar (){
+        System.out.println("Sonando" + this.titulo);
+    }
+}
